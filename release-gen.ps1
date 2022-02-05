9ca06a4f2c57ddfc27847bdd33eb88010d23237a
lysychenko <98894503+lysychenko@users.noreply.github.com>
2022-02-05 23:30:02 +0300

    Update release-gen.ps1

--- release-gen.ps1
+++ release-gen.ps1
@@ -29,7 +29,7 @@ $github = New-Module -ScriptBlock {
 
 Write-Host ("Getting all commits from git tag v" + $last_release_version + " to commit sha $current_commitId.")
 
-$response = $github.GetCommits($release.Version, $current_commitId)
+$response = $github.GetCommits($release_version, $current_commitId)
 
 $commits = $response.commits | Sort-Object -Property @{Expression={$_.commit.author.date}; Ascending=$false} -Descending
 
